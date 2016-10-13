81ecdb17b15079bcd182775ee84107daef346ed7
Mokai0 <mokai86@gmail.com>
2016-10-13 17:13:52 -0400

    started battle.html

--- RandomFighter/app/monster.js
+++ RandomFighter/app/monster.js
@@ -1,14 +1,19 @@
 //Monster counter
 var MC = 0
 
-for (i = 0; i < '../img/monsters'.length; i++) {
-    document.getElementById('mpic') += cars[i] + "<br>";
-}
-document.getElementById('selectedFighter').innerHTML += "you've clicked a button";
-do {
-  MC++;
+// for (i = 0; i < '../img/monsters'.length; i++) {
+//     document.getElementById('mpic') += cars[i] + "<br>";
+// }
 
-    text += "The number is " + i;
-    i++;
+// do {
+//   MC++;
+//
+//     text += "The number is " + i;
+//     i++;
+// }
+// while (i < 10);
+
+for (MC; MC<'../img/monsters'.length; MC++) {
+  console.log(MC);
+  document.write(img/monsters.MC);
 }
-while (i < 10);
