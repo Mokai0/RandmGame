7316d39eb76308bc447691261a3c0a336c14a3a9
Mokai0 <mokai86@gmail.com>
2016-11-18 11:06:27 -0500

    working now

--- RandomFighter/public/app.js
+++ RandomFighter/public/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
 var express = require('express');
-
 var app = express();
+var path = require('path');
 
-app.use('/', express.static('public'));
+app.use('/', express.static(path.join(__dirname, '../public')));
 
 app.listen(3000, function() {
   console.log('serving');
